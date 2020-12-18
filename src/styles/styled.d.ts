9f96ebf21795598d4ab0e38fa04e97156c22611c
Pedro Marques <pedromarques1011@gmail.ocm>
2020-12-17 22:20:28 -0300

    added:arq de definiçao de tipo, theme light, header, initial setup

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,18 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    title: string;
+
+    colors: {
+      primary: string;
+      secundary: string;
+
+      background: string;
+      frontItems: string,
+      text: string;
+    };
+  }
+}
+
+// arquivo de definição de tipo / definição de tipo global
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,12 @@
+export default {
+  title: "light",
+
+  colors: {
+    primary: "#4236CF",
+    secundary: "#ff7904",
+
+    background: "#DFDDF4",
+    frontItems: "#F0EFFF",
+    text: "#1C1A33",
+  },
+};
