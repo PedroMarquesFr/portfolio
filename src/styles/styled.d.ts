bcac93f3754e228aac0b2f973ac84b2a0f51e1ec
Pedro Marques <pedromarques1011@gmail.ocm>
2020-12-23 14:40:00 -0300

    anotations

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -9,11 +9,27 @@ declare module "styled-components" {
       secundary: string;
 
       background: string;
-      frontItems: string,
+      frontItems: string;
       text: string;
     };
-    toggleTheme:()=>void;
+    toggleTheme: () => void;
   }
 }
 
 // arquivo de definição de tipo / definição de tipo global
+
+//o styled components usa para seuo setup de um tema padrao o ThemeProvider, um contextAPI.
+//Voce chama essa context API com themeProvider
+// que é aquivalente ao contextProvider de um contextAPI qualquer, ate a achamda e semalhante:
+// <ThemeProvider theme={{...theme, toggleTheme}}>
+//  ...
+// </ThemeProvider> ou <GenectContext.Provider value={}></GenectContext.Provider>
+
+//Getting the theme without styled components:
+//Para pegar: se trata de um contextAPI normal, então, pode-se o acessar com useContext
+// import { ThemeContext } from 'styled-components';
+
+//obs: o ThemeContext foi declarado em algum lugar o codigo do styldComps assim: const ThemeContext = React.createContext();
+//obs: e em um arquivo chamado ThemeProvider tem algo do tipo <ThemeContext.Provider>{props.children}</ThemeContext.Provider>
+
+//lembrando que para o uso exclusivo do style components nao precisa disso pois ele o objeto do tema pode ser pego via props
