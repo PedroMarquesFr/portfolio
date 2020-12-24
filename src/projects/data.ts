d1ab30e060a30154d9b9975d1e9e0273be502578
Pedro Marques Fr <pedromarques1011@gmail.com>
2020-12-24 17:47:52 -0300

    atts

--- src/projects/data.ts
+++ src/projects/data.ts
@@ -1,14 +1,29 @@
-export const porjects = [
+export const allLearnedThings: string[] = ["js", "css", "html", "react", "sc"];
+
+export const projects = [
   {
     instituition: "Trybe",
     title: "Frontend Online Store",
     description:
       "In this project, I was able to solidify the concepts of state and component plus routes in React",
+    tags: ["js", "css", "html", "react", "sc"],
   },
   {
     instituition: "Trybe",
     title: "blalalaab",
-    description:
-      "lorsaasdasdasdadem",
+    description: "lorsaasdasdasdademgfdsfdsfdsfdsfdsfdsf",
+    tags: ["js", "css", "html", "react", "sc"],
+  },
+  {
+    instituition: "Trybe",
+    title: "blalalaab",
+    description: "lorsaasdasdasdademgfdsfdsfdsfdsfdsfdsf",
+    tags: ["js", "css", "html", "react", "sc"],
+  },
+  {
+    instituition: "Trybe",
+    title: "blalalaab",
+    description: "lorsaasdasdasdademgfdsfdsfdsfdsfdsfdsf",
+    tags: ["js", "css", "html", "react", "sc"],
   },
 ];
