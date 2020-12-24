8258287609d7cc62c55983e5532a34d1298f6d42
Pedro Marques Fr <pedromarques1011@gmail.com>
2020-12-24 15:24:24 -0300

    tags done first part

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -8,6 +8,13 @@ const light =  {
     background: "#DFDDF4",
     frontItems: "#F0EFFF",
     text: "#56546D",
+    textsec: "#1C1A33",
+
+    react: "#0195E8",
+    html: "#ED5601",
+    css: "#1044F9",
+    js: "#E5B300",
+    sc: "#F22A4E",
   },
 };
 
