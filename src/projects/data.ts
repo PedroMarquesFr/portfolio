1216d4122db91c338594851e5b31b874820b25eb
Pedro Marques Fr <pedromarques1011@gmail.com>
2020-12-27 16:13:33 -0300

    is getting good :D

--- src/projects/data.ts
+++ src/projects/data.ts
@@ -3,34 +3,44 @@ export const allLearnedThings: string[] = ["js", "css", "html", "react", "sc"];
 export const projects = [
   {
     instituition: "Trybe",
+    title: "Movie Card Library CRUD",
+    description:
+      "In this project, I was able to solidify the concepts of state and component plus routes in React creating a CRUD (Create,  Read, Update, Delete)",
+    tags: ["js", "css", "html", "react"],
+    pathname:
+      "https://github.com/PedroMarquesFr/project-movie-card-library-crud",
+  },
+  {
+    instituition: "Me",
+    title: "My Portfolio",
+
+    description:
+      "My first intense experience using TypeScript with React, as I was already exposed to strongly typed languages like C and JAVA in college, it was easier to understand Typescript. It was very important to practice styled components, in which I was a few months away from training becouse of Trybe tight schedule.",
+    tags: ["js", "css", "html", "react", "sc"],
+    pathname: "https://github.com/PedroMarquesFr/portfolio",
+  },
+  {
+    instituition: "RocketSeat",
+    title: "Twitter Clone",
+    description:
+      "A Twitter UI clone using Styled Components, one of my first design projects. In this project, I was able to put the Styled Components library into practice and solidify various CSS concepts",
+    tags: ["js", "css", "html", "react", "sc"],
+    pathname: "https://github.com/PedroMarquesFr/twitter-clone",
+  },
+  {
+    instituition: "Trybe",
+    title: "Async Shopping Cart",
+    description:
+      "A simple Online store that simulates shopping behavior using Mercado livre API, with search mechanism. Besides all javaScript concepts that I put into practice, it was so cool to transform the original proposed design and UX into something that, at the time, I loved!",
+    tags: ["js", "css", "html"],
+    pathname: "https://github.com/PedroMarquesFr/shopping-cart",
+  },
+  {
+    instituition: "",
     title: "Frontend Online Store",
     description:
-      "In this project, I was able to solidify the concepts of state and component plus routes in React",
+      "In my first group project, my team used Agile Methodologies like Kanban and Scrum. In this project, I have learned that sometimes people are harder than code and that if all the group members are not on the same page, the project will fail,  but I know that the power of an organized group can be insane. The project itself is kind of an evolution of the Async Shopping Cart project, with more features, like choose the amount of each item, detail page with rate possibility, checkout page, payment methods. I refactored the code to use Redux, so now it has a better performance",
     tags: ["js", "css", "html", "react", "sc"],
-    pathname: "https://github.com/PedroMarquesFr/shopping-cart",
-  },
-  {
-    instituition: "Trybe",
-    title: "blalalaab",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam ipsum temporibus eligendi pariatur vel ut fugiat quisquam voluptas. Architecto tenetur nulla corporis nobis cumque perspiciatis non libero sed nemo qui!",
-    tags: ["js", "css", "html", "react", "sc"],
-    pathname: "https://github.com/PedroMarquesFr/shopping-cart",
-  },
-  {
-    instituition: "Trybe",
-    title: "blalalaab",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, nihil labore animi asperiores ad neque cum velit sunt, repellat culpa dolores doloremque, illo eius! Laboriosam beatae veniam aspernatur repellendus sint?",
-    tags: ["js", "css", "html", "react", "sc"],
-    pathname: "https://github.com/PedroMarquesFr/shopping-cart",
-  },
-  {
-    instituition: "Trybe",
-    title: "blalalaab",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, nihil labore animi asperiores ad neque cum velit sunt, repellat culpa dolores doloremque, illo eius! Laboriosam beatae veniam aspernatur repellendus sint?",
-    tags: ["js", "css", "html", "react", "sc"],
-    pathname: "https://github.com/PedroMarquesFr/shopping-cart",
+    pathname: "",
   },
 ];
